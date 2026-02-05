50b9180fa0c645fb62a18870f8ccca77b8da294d
ddevedzic1 <ddevedzic1@etf.unsa.ba>
2026-02-05 17:10:47 +0100

    Izmijenjen maksimalan broj čvorova i težina grana

--- include/constants.h
+++ include/constants.h
@@ -2,8 +2,8 @@
 
 constexpr int INF = 1'000'000'000;
 
-constexpr int MAX_N = 16385;
+constexpr int MAX_N = 32769;
 
-constexpr int MAX_EDGE_WEIGHT = 50'000;
+constexpr int MAX_EDGE_WEIGHT = 25'000;
 
 using WeightType = int;
